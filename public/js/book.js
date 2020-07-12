5a9172d0055626ae83c060cbf400af9a27cfecd3
abdulelahxd <abdulelahkassara@gmail.com>
2020-07-13 02:10:27 +0300

    newUpdate

---
+++ public/js/book.js
@@ -0,0 +1,17 @@
+function burger(){
+    var burger = document.getElementById('burger');
+    var links = document.getElementById('links');
+    var quit = document.getElementById('quit');
+    burger.style.padding = '12px 12px 150vw 150vw';
+    links.style.display = 'flex';
+    quit.style.display = 'inline';
+  }
+  
+  function quit(){
+    var burger = document.getElementById('burger');
+    var links = document.getElementById('links');
+    var quit = document.getElementById('quit');
+    burger.style.padding = '4px 4px';
+    links.style.display = 'none';
+    quit.style.display = 'none';
+  }
