c6ecc8ac1245b1d1275dcbf3993d9a422fe28404
abdulelahxd <abdulelahkassara@gmail.com>
2020-07-14 17:03:49 +0300

    some Edits

--- data/books.sql
+++ data/books.sql
@@ -2,9 +2,9 @@ DROP TABLE IF EXISTS booksdb;
 
 CREATE TABLE IF NOT EXISTS booksdb (
     id SERIAL PRIMARY KEY,
-    author VARCHAR(255),
-    title VARCHAR(255),
-    ISBN VARCHAR(255),
+    author VARCHAR(500),
+    title VARCHAR(500),
+    ISBN VARCHAR(500),
     image_url TEXT,
     description TEXT,
     bookshell VARCHAR(500)
